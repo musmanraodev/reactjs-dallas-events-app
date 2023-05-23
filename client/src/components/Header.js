3ecc06050e8e44c450781fba67a9e30fbfe7c959
Muhammad Usman <musmanraodev@gmail.com>
2023-05-23 13:42:32 -0400

    updated

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -79,7 +79,7 @@ class Header extends Component {
                   {this.renderLogoutLink()}
                 </ul>
               </li>
-              <li onClick={() => window.open("https://musmanrao1994.github.io/")} >
+              <li onClick={() => window.open("https://musmanraoDev.github.io/")} >
                 Portfolio
               </li>
             </ul>
